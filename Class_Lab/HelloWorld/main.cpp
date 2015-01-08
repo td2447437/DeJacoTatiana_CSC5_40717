2546603ce3a748b98dbfeae05266296d0f8ef59e
Tatiana DeJaco <tjdejaco@gmail.com>
2015-01-07 19:12:16 -0800

    Updating Class_Lab

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Tati
+ * Created on January 5, 2015, 10:26 AM
+ * Purpose: First Program
+ * 
+ * Testing GITHUB
+ */
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Global Constants
+
+// Function Prototypes
+
+// Execution Begins Here
+int main(int argc, char** argv) 
+{
+    // Output Hello World
+    cout << "Hello World!\n";
+    // cout << "My name is Tatiana ";
+    
+    return 0;
+}
+
