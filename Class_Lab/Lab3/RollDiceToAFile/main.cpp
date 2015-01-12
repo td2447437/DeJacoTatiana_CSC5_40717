964867988372b6230e4ebaf798dc200d62f16f87
Tatiana DeJaco <tjdejaco@gmail.com>
2015-01-12 10:08:09 -0800

    Create new file

---
+++ Class_Lab/Lab3/RollDiceToAFile/main.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.cpp
+ * Author: Tati
+ * Created on January 12, 2015, 10:04 AM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+
+    return 0;
+}
+
